1748a41e254c4a41a9657efc05a495765106d92f
Geborgen <88332109+Geborgen@users.noreply.github.com>
2022-10-29 13:30:41 -0400

    Create modlists.js

---
+++ modlists.js
@@ -0,0 +1,41 @@
+[
+  {
+    "$type": "ModListMetadata, Wabbajack.Lib",
+    "title": "Nordic Souls",
+    "description": "Modlist providing a visual and gameplay overhaul designed to 'freshen up' and streamline Skyrim, while taking inspirations from other games yet still staying true to Skyrim's Nordic theme.",
+    "author": "Geborgen",
+    "maintainers": [
+      "github/Geborgen"
+    ],
+    "game": "skyrimspecialedition",
+    "tags": [
+      "AE",
+      "CC Content",
+      "LoTD",
+      "SimonRim",
+      "Valhalla Combat",
+      "NeverNude" 
+    ],
+    "nsfw": false,
+    "image_contains_title": false,
+    "force_down": false,
+    "links": {
+      "$type": "Links, Wabbajack.Lib",
+      "image": "https://raw.githubusercontent.com/Geborgen/nordic-souls/main/.github/NSLOGO.png",
+      "readme": "https://github.com/Geborgen/nordic-souls/blob/main/README.md",
+      "download": "https://authored-files.wabbajack.org/Halgaris Helper.wabbajack_29d245c9-5f7f-4394-b600-a22e43427337",
+      "machineURL": "nordicsouls",
+      "discordURL": "https://discord.com/invite/nsJquPrHHE"
+    },
+    "download_metadata": {
+      "$type": "DownloadMetadata, Wabbajack.Lib",
+      "Hash": "jJxPbgTMnQM=",
+      "Size": 399522536,
+      "NumberOfArchives": 1210,
+      "SizeOfArchives": 103567429761,
+      "NumberOfInstalledFiles": 151708,
+      "SizeOfInstalledFiles": 150182263210
+    },
+    "version": "1.0"
+  }
+]
